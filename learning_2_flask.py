0d261b2e26cab19e79dad7f1c7c443e335275ee2
d1658999 <d1658999@gmail.com>
2022-01-28 23:58:54 +0800

    flask basic v2

--- learning_2_flask.py
+++ learning_2_flask.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log', 'r') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
 
 
 # @app.route('/')
@@ -15,6 +35,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html',
                            the_phrase=phrase,
                            the_letters=letters,
